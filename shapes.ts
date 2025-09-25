a63c71219fe0e320e658907dcf1c249eeb1098e6
Temetsio <alankoteemu12@gmail.com>
2025-09-25 09:26:48 +0300

    Add files via upload

---
+++ shapes.ts
@@ -0,0 +1,19 @@
+export class Rectangle {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+
+  constructor(x: number, y: number, width: number, height: number) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.stroke();
+  }
+}
