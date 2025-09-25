d8103a541c066f16207945ba4469c072fed48e92
Temetsio <alankoteemu12@gmail.com>
2025-09-25 09:30:50 +0300

    Update shapes.ts

--- shapes.ts
+++ shapes.ts
@@ -17,3 +17,21 @@ export class Rectangle {
     ctx.stroke();
   }
 }
+export class Circle {
+  x: number;
+  y: number;
+  radius: number;
+
+  constructor(x: number, y: number, radius: number) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.stroke();
+  }
+}
+
