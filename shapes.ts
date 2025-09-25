4600dd3e67d5a3590c0aa6ff216ef4ebd35cf12a
Temetsio <alankoteemu12@gmail.com>
2025-09-25 10:32:16 +0300

    Update shapes.ts

--- shapes.ts
+++ shapes.ts
@@ -87,6 +87,62 @@ export abstract class Shape {
   }
   abstract draw(ctx: CanvasRenderingContext2D): void;
 }
+export abstract class Shape {
+  style: string;
+
+  constructor(style: string = 'black') {
+    this.style = style;
+  }
+
+  abstract draw(ctx: CanvasRenderingContext2D): void;
+
+  toString(): string {
+    return `Shape with style ${this.style}`;
+  }
+}
+export class Rectangle extends Shape {
+  location: Point;
+  size: Size;
+
+  constructor(x: number, y: number, width: number, height: number, style: string = 'black') {
+    super(style);
+    this.location = new Point(x, y);
+    this.size = new Size(width, height);
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.strokeStyle = this.style;
+    ctx.rect(this.location.x, this.location.y, this.size.width, this.size.height);
+    ctx.stroke();
+  }
+
+  override toString(): string {
+    return `Rectangle with location (${this.location.x}, ${this.location.y}), size (${this.size.width} x ${this.size.height}) and style ${this.style}`;
+  }
+}
+export class Circle extends Shape {
+  center: Point;
+  radius: number;
+
+  constructor(x: number, y: number, radius: number, style: string = 'black') {
+    super(style);
+    this.center = new Point(x, y);
+    this.radius = radius;
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.strokeStyle = this.style;
+    ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+    ctx.stroke();
+  }
+
+  override toString(): string {
+    return `Circle with center (${this.center.x}, ${this.center.y}), radius ${this.radius} and style ${this.style}`;
+  }
+}
+
 
 
 
