d16f251605c47002480162cc41c46dbce08a654e
Temetsio <alankoteemu12@gmail.com>
2025-10-02 09:20:56 +0300

    Create Point.ts

---
+++ Point.ts
@@ -0,0 +1,17 @@
+export class Point {
+  private _x: number;
+  private _y: number;
+
+  public constructor(x: number, y: number) {
+    this._x = x;
+    this._y = y;
+  }
+
+  public get x(): number {
+    return this._x;
+  }
+
+  public get y(): number {
+    return this._y;
+  }
+}
