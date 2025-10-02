ffbe8c27168eadfd34ccae4eea8f6a7bc316068f
Temetsio <alankoteemu12@gmail.com>
2025-10-02 09:33:00 +0300

    Update Size.ts

--- Size.ts
+++ Size.ts
@@ -15,3 +15,22 @@ export class Size {
     return this._height;
   }
 }
+export class Size {
+
+  private _width: number;
+
+  private _height: number;
+  public constructor(width: number, height: number) {
+    this._width = width;
+    this._height = height;
+  }
+
+
+  public get width(): number {
+    return this._width;
+  }
+
+  public get height(): number {
+    return this._height;
+  }
+}
