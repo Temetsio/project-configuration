0fd882640281429590ca89ca2904d859cfe17507
Temetsio <alankoteemu12@gmail.com>
2025-11-10 11:00:07 +0200

    Clean up shape-viewer.ts by removing comments
    Removed commented-out code and unnecessary imports.

---
+++ src/shape-viewer.ts
@@ -0,0 +1,5 @@
+import { ShapeViewer } from "./shape-actions";
+import { PaletteComponent } from "./shape-palette";
+import { SelectAction, AddShapeAction } from "./shape-actions";
+import { Rectangle, Circle, Triangle } from "./shapes"; 
+
